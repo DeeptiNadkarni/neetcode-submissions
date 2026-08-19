467f9d28d3e21bd408902b1b723a2bb5e23d4829
DeeptiNadkarni <35568415+DeeptiNadkarni@users.noreply.github.com>
2026-08-19 19:56:06 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 11.8
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Eleven"
+print(type(variable))
+variable = [12, 14, 16, 18, 20]
+print(type(variable))
